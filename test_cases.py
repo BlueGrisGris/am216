8ba91ee1d17e19059ff4b13de523e9e971d4365a
BlueGrisGris <emanninen@college.harvard.edu>
2022-04-29 11:04:09 -0400

    1d test cases

---
+++ test_cases.py
@@ -0,0 +1,45 @@
+
+
+
+
+#|%%--%%| <FCXglLHwTU|2BShAsWM7q>
+"""°°°
+## Test Cases for Acoustic inversion
+°°°"""
+#|%%--%%| <2BShAsWM7q|xcJT3wzdbH>
+"""°°°
+### 1D Case
+°°°"""
+#|%%--%%| <xcJT3wzdbH|Jm1q1Tbh15>
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+#|%%--%%| <Jm1q1Tbh15|dtKEgEI44k>
+### monotonic linear speed function 
+max_speed = 1.5
+min_speed = 0.5
+NN = 100 
+
+linear_test = np.linspace(min_speed, max_speed, NN) 
+#|%%--%%| <dtKEgEI44k|E04g3KfLfC>
+### step speed function
+step_test = np.zeros_like(linear_test)
+step_test[0:40] = 0.5
+step_test[61:99] = 0.5
+step_test[41:60] = 0.5
+
+#|%%--%%| <E04g3KfLfC|a0ykRqVkiG>
+### sin low freq speed function
+sin_low_test = np.sin(linear_test*np.pi)
+plt.plot(linear_test, sin_test)
+plt.show()
+
+#|%%--%%| <a0ykRqVkiG|Sm7hxnGAJH>
+### sin high freq speed function
+sin_high_test = np.sin(linear_test*6*np.pi)
+plt.plot(linear_test, sin_high_test)
+plt.show()
+
+
+
